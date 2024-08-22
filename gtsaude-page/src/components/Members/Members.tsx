c41a7ea840f6a8b62f159c57f5daadc4d5b8cf18
Lara <lara.lbraga@alu.ufc.br>
2024-08-22 14:26:11 -0300

    :sparkles: feat: added members page

--- gtsaude-page/src/components/Members/Members.tsx
+++ gtsaude-page/src/components/Members/Members.tsx
@@ -1,6 +1,19 @@
+import CardMembersCarousel from "../UI/CardMembersCarousel";
+
 const Members = () => {
     return (
-        0
+        <div className="flex flex-col items-center bg-primary-white h-full py-8 gap-12 w-full">
+            <div className="flex flex-col items-center gap-6 max-w-4xl py-4">
+                <h1 className="text-xl md:text-4xl text-primary-blue text-center font-semibold">Membros</h1>
+                <p className="text-base md:text-xl text-primary-blue text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quod et, nisi suscipit cupiditate animi reprehenderit ipsam ipsum, itaque alias corrupti
+                    repellendus asperiores vitae est consectetur odit expedita veniam officiis! Cupiditate.
+                </p>
+            </div>
+            <div className="flex">
+                <CardMembersCarousel />
+            </div>
+        </div>
     );
 }
 
--- gtsaude-page/src/components/Research/Research.tsx
+++ gtsaude-page/src/components/Research/Research.tsx
@@ -1,6 +1,13 @@
 const Research = () => {
     return (
-        0
+        <div className="flex flex-col items-center h-full py-8 gap-12 bg-primary-blue">
+            <div>
+                <h1>Pesquisas em Andamento</h1>
+                <p>Esta seção oferece aos visitantes uma oportunidade de acompanhar os avanços mais recentes e inovadores em diversas áreas do conhecimento. Cada projeto listado inclui uma descrição concisa de seus objetivos, metodologia e impacto esperado, proporcionando um panorama das questões científicas e tecnológicas que estamos explorando.</p>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
     );
 }
 
